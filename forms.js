5bd6bbbf6b5488219bf038baa34ba811a4011bf7
João Victor <devsjoaovvictor@gmail.com>
2023-05-30 16:50:51 -0300

    criando bolinha

--- forms.js
+++ forms.js
@@ -8,3 +8,35 @@ let larguraPalco = palco.offsetWidth
 let alturaPalco = palco.offsetHeight
 let bolas = []
 let numBola = 0
+
+class Bola {
+   constructor(){
+    this.tam = Math.floor(Math.random()*15)+10
+    this.r = Math.floor(Math.random()*255)
+    this.g = Math.floor(Math.random()*255)
+    this.b = Math.floor(Math.random()*255)
+    this.px = Math.floor(Math.random()(larguraPalco-this.tam))
+    this.py = Math.floor(Math.random()(alturaPalco-this.tam))
+   }
+
+}
+
+
+
+window.addEventListener("resize",(evt)=>{
+    larguraPalco = palco.offsetWidth
+    alturaPalco = palco.offsetHeight
+})
+
+btn_add.addEventListener("click",(evt)=>{
+    const qtde = txt_qtde.value
+    for(let i=0;i<qtde;i++){
+      
+    }
+})
+
+btn_remover.addEventListener("click",(evt)=>{
+    bolas.map((b)=>{
+     
+    })
+})
