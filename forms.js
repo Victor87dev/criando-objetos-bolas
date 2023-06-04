d03577042475a289b8870469a375d250511b37a9
João Victor <devsjoaovvictor@gmail.com>
2023-06-04 14:15:03 -0300

    adicionando mudança de cor

--- forms.js
+++ forms.js
@@ -3,11 +3,98 @@ const num_objetos = document.getElementById("num_objetos")
 const txt_qtde = document.getElementById("txt_qtde")
 const btn_add = document.getElementById("btn_add")
 const btn_remover = document.getElementById("btn_remover")
+const radio_blue = document.getElementById("radioBlue")
+const radio_red = document.getElementById("radioRed")
+const radio_green = document.getElementById("radioGreen")
+const radio_colorFul = document.getElementById("radioColorFul")
 
 let larguraPalco = palco.offsetWidth
 let alturaPalco = palco.offsetHeight
 let bolas = []
 let numBola = 0
+
+let blue = false
+let red = false 
+let green = false 
+let colorFul = false
+let add = false 
+
+radio_blue.addEventListener("click",(evt)=>{
+if(!add){
+  blue = true
+  red = false
+  green = false 
+  colorFul = false 
+}else if(add && blue){
+  radio_blue.checked = true
+}else if(add && red){
+  blue = false
+  radio_red.checked = true
+}else if(add && green){
+  blue = false
+  radio_green.checked = true
+}else if(add && colorFul){
+  blue = false
+  radio_colorFul.checked = true
+}
+})
+radio_red.addEventListener("click",(evt)=>{
+if(!add){
+  blue = false
+  red = true
+  green = false 
+  colorFul = false 
+}else if(add && red){
+  radio_red.checked = true
+}else if(add && blue){
+  red = false
+  radio_blue.checked = true
+}else if(add && green){
+  red = false
+  radio_green.checked = true
+}else if(add && colorFul){
+  red = false
+  radio_colorFul.checked = true
+}
+})
+radio_green.addEventListener("click",(evt)=>{
+if(!add){
+  blue = false
+  red = false
+  green = true 
+  colorFul = false
+}else if(add && green){
+  radio_green.checked = true
+}else if(add && red){
+  green = false
+  radio_red.checked = true
+}else if(add && blue){
+  green = false
+  radio_blue.checked = true
+}else if(add && colorFul){
+  green = false
+  radio_colorFul.checked = true
+}
+})
+radio_colorFul.addEventListener("click",(evt)=>{
+if(!add){
+  blue = false
+  red = false
+  green = false 
+  colorFul = true 
+}else if(add && colorFul){
+  radio_colorFul.checked = true
+}else if(add && red){
+  colorFul = false
+  radio_red.checked = true
+}else if(add && green){
+  colorFul = false
+  radio_green.checked = true
+}else if(add && blue){
+  colorFul = false
+  radio_blue.checked = true
+}  
+})
 
 class Bola {
    constructor(arrayBolas,palco){
@@ -48,12 +135,32 @@ class Bola {
    }
 
    desenhar=()=>{
-     const div = document.createElement("div")
-     div.setAttribute("id",this.id)
-     div.setAttribute("class","bola")
-     div.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
-     this.palco.appendChild(div)
-   }
+    if(red){
+      const div = document.createElement("div")
+      div.setAttribute("id",this.id)
+      div.setAttribute("class","bola")
+      div.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(255,0,0)`)
+      this.palco.appendChild(div)
+    }else if(blue){
+      const div = document.createElement("div")
+      div.setAttribute("id",this.id)
+      div.setAttribute("class","bola")
+      div.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(17,0,255)`)
+      this.palco.appendChild(div)
+    }else if(green){
+      const div = document.createElement("div")
+      div.setAttribute("id",this.id)
+      div.setAttribute("class","bola")
+      div.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(43,255,0)`)
+      this.palco.appendChild(div)
+    }else if(colorFul){
+      const div = document.createElement("div")
+      div.setAttribute("id",this.id)
+      div.setAttribute("class","bola")
+      div.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
+      this.palco.appendChild(div)
+    }  
+}
 
    controle_bordas=()=>{
      if(this.px+this.tam >= larguraPalco){
@@ -69,10 +176,27 @@ class Bola {
    }
 
    controlar=()=>{
+    if(red){
+      this.controle_bordas()
+      this.px+=this.dirx*this.velx
+      this.py+=this.diry*this.vely
+      this.eu.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(255,0,0)`)
+    }else if(blue){
+      this.controle_bordas()
+      this.px+=this.dirx*this.velx
+      this.py+=this.diry*this.vely
+      this.eu.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(17,0,255)`)
+    }else if(green){
+      this.controle_bordas()
+      this.px+=this.dirx*this.velx
+      this.py+=this.diry*this.vely
+      this.eu.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(43,255,0)`)
+    }else{
       this.controle_bordas()
       this.px+=this.dirx*this.velx
       this.py+=this.diry*this.vely
       this.eu.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
+    }
       if((this.px > larguraPalco) || (this.py > alturaPalco)) {
          this.remover()
       }
@@ -85,14 +209,36 @@ window.addEventListener("resize",(evt)=>{
 })
 
 btn_add.addEventListener("click",(evt)=>{
-    const qtde = txt_qtde.value
-    for(let i=0;i<qtde;i++){
-     bolas.push(new Bola(bolas,palco))
+  add = true
+    if(txt_qtde.value == "0"){
+      alert("adicione alguma quantidade")
+      add = false
     }
+    if(!red && !blue && !green && !colorFul && txt_qtde.value!="0"){
+      alert("selecione uma cor")
+      num_objetos.innerHTML = 0
+      add = false
+    }else{
+      const qtde = txt_qtde.value
+      for(let i=0;i<qtde;i++){
+      bolas.push(new Bola(bolas,palco))
+    }
+    }  
+    
 })
 
 btn_remover.addEventListener("click",(evt)=>{
     bolas.map((b)=>{
      b.remover()
     })
+txt_qtde.value = "0"    
+add = false
+blue = false
+red = false 
+green = false 
+colorFul = false 
+radio_green.checked = false
+radio_blue.checked = false
+radio_red.checked = false
+radio_colorFul.checked = false
 })
